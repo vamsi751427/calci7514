8855f62600515f1a89bc3f5dce472b1f9c69961c
vamsi751427 <128027906+vamsi751427@users.noreply.github.com>
2023-03-23 13:53:01 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+exports.add=add;
+exports.sub=sub;
+exports.mul=mul;
+exports.div=div;
